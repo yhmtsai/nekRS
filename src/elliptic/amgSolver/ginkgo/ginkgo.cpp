13755aecab3779c03dacd5b3bdcc5fab097570df
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2023-05-02 21:30:40 +0200

    ginkgo mpi version

--- src/elliptic/amgSolver/ginkgo/ginkgo.cpp
+++ src/elliptic/amgSolver/ginkgo/ginkgo.cpp
@@ -11,7 +11,9 @@
 #include <memory>
 
 static std::shared_ptr<gko::LinOp> solver;
-
+static int64_t num_local_rows;
+static int64_t num_global_rows;
+static MPI_Comm dup_comm;
 void GinkgoSolver_setup(const int nLocalRows,
                         const int nnz,
                         const long long *rows,
@@ -23,26 +25,53 @@ void GinkgoSolver_setup(const int nLocalRows,
 {
   printf("Ginkgo: build solver\n");
   fflush(stdout);
-  static_assert(sizeof(int64_t) == 8, "int64_t");
-  static_assert(sizeof(long long) == 8, "long long");
-  static_assert(sizeof(long) == 8, "long");
   using IndexType = int64_t;
   using ValueType = double;
-  auto exec =
-      gko::CudaExecutor::create(0, gko::ReferenceExecutor::create(), false, gko::allocation_mode::device);
+  auto exec = gko::CudaExecutor::create(deviceID,
+                                        gko::ReferenceExecutor::create(),
+                                        false,
+                                        gko::allocation_mode::device);
+  MPI_Comm_dup(comm, &dup_comm);
+  int mpi_rank = 0;
+  int mpi_size = 0;
+  MPI_Comm_rank(dup_comm, &mpi_rank);
+  MPI_Comm_size(dup_comm, &mpi_size);
+  auto matrix_host =
+      gko::share(gko::experimental::distributed::Matrix<ValueType, int, IndexType>::create(exec->get_master(),
+                                                                                           dup_comm));
   auto rows_view =
       gko::array<IndexType>::const_view(exec->get_master(), nnz, reinterpret_cast<const IndexType *>(rows));
   auto cols_view =
       gko::array<IndexType>::const_view(exec->get_master(), nnz, reinterpret_cast<const IndexType *>(cols));
   auto vals_view = gko::array<ValueType>::const_view(exec->get_master(), nnz, values);
-  //   one node nLocalCols = nLocalRows
-  auto coo = gko::matrix::Coo<ValueType, IndexType>::create_const(exec->get_master(),
-                                                                  gko::dim<2>{nLocalRows, nLocalRows},
-                                                                  std::move(vals_view),
-                                                                  std::move(cols_view),
-                                                                  std::move(rows_view));
-  auto matrix = gko::share(gko::matrix::Csr<ValueType, IndexType>::create(exec));
-  matrix->copy_from(coo.get());
+  gko::array<IndexType> all_num_rows(exec->get_master(), mpi_size + 1);
+  num_local_rows = nLocalRows;
+  MPI_Allgather(&num_local_rows, 1, MPI_LONG_LONG, all_num_rows.get_data() + 1, 1, MPI_LONG_LONG, dup_comm);
+  all_num_rows.get_data()[0] = 0;
+  for (int r = 2; r <= mpi_size; r++) {
+    all_num_rows.get_data()[r] += all_num_rows.get_data()[r - 1];
+  }
+  num_global_rows = all_num_rows.get_const_data()[mpi_size];
+  auto data =
+      gko::device_matrix_data<ValueType, IndexType>(exec->get_master(),
+                                                    gko::dim<2>{num_global_rows, num_global_rows},
+                                                    gko::detail::array_const_cast(std::move(rows_view)),
+                                                    gko::detail::array_const_cast(std::move(cols_view)),
+                                                    gko::detail::array_const_cast(std::move(vals_view)));
+  auto partition =
+      gko::experimental::distributed::Partition<int, IndexType>::build_from_contiguous(exec->get_master(),
+                                                                                       all_num_rows);
+  matrix_host->read_distributed(data, partition.get());
+
+  // auto coo = gko::matrix::Coo<ValueType, IndexType>::create_const(exec->get_master(),
+  //                                                                 gko::dim<2>{nLocalRows, nLocalRows},
+  //                                                                 std::move(vals_view),
+  //                                                                 std::move(cols_view),
+  //                                                                 std::move(rows_view));
+  // auto matrix = gko::share(gko::matrix::Csr<ValueType, IndexType>::create(exec));
+  auto matrix =
+      gko::share(gko::experimental::distributed::Matrix<ValueType, int, IndexType>::create(exec, comm));
+  matrix->copy_from(matrix_host.get());
   solver = gko::share(
       gko::solver::Cg<ValueType>::build()
           .with_criteria(gko::stop::Iteration::build().with_max_iters(100u).on(exec))
@@ -52,18 +81,27 @@ void GinkgoSolver_setup(const int nLocalRows,
 
 template <typename ValueType> void GinkgoSolver_solve(ValueType *x, ValueType *rhs)
 {
-  printf("Ginkgo: solve\n");
+  printf("Ginkgo: solve global %ld local %ld \n", num_global_rows, num_local_rows);
   fflush(stdout);
   // on gpu
   auto exec = solver->get_executor();
-  auto n = solver->get_size()[0];
-  auto x_view = gko::array<ValueType>::view(exec, n, x);
-  auto rhs_view = gko::array<ValueType>::const_view(exec, n, rhs);
-  auto dense_x = gko::matrix::Dense<ValueType>::create(exec, gko::dim<2>{n, 1}, std::move(x_view), 1);
+  auto x_view = gko::array<ValueType>::view(exec, num_local_rows, x);
+  auto rhs_view = gko::array<ValueType>::view(exec, num_local_rows, rhs);
+  auto dense_x =
+      gko::matrix::Dense<ValueType>::create(exec, gko::dim<2>{num_local_rows, 1}, std::move(x_view), 1);
   auto dense_rhs =
-      gko::matrix::Dense<ValueType>::create_const(exec, gko::dim<2>{n, 1}, std::move(rhs_view), 1);
-  solver->apply(dense_rhs.get(), dense_x.get());
+      gko::matrix::Dense<ValueType>::create(exec, gko::dim<2>{num_local_rows, 1}, std::move(rhs_view), 1);
+  auto par_x = gko::experimental::distributed::Vector<ValueType>::create(exec,
+                                                                         dup_comm,
+                                                                         gko::dim<2>{num_global_rows, 1},
+                                                                         gko::give(dense_x).get());
+  auto par_rhs = gko::experimental::distributed::Vector<ValueType>::create(exec,
+                                                                           dup_comm,
+                                                                           gko::dim<2>{num_global_rows, 1},
+                                                                           gko::give(dense_rhs).get());
+  solver->apply(par_rhs.get(), par_x.get());
 }
+
 void GinkgoSolver_solve(void *x, void *rhs)
 {
   // useFP32 -> x, rhs is float
